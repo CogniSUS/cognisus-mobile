3f361058da6b3c23e9e1f6452751a7486deb906e
Lucas Levy <lucaslevy@id.uff.br>
2026-04-17 18:11:59 -0300

    implementação do layout e atualização de logos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,26 @@
-import { Stack } from "expo-router";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
+import { StyleSheet, View } from "react-native";
+import { AppHeader } from "@/components/layout/app-header";
+import { MenuApp } from "@/components/layout/menu-app";
 
-export default function RootLayout() {
+export default function AppLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          headerTitleAlign: "center",
-        }}
-      />
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <AppHeader />
+      <View style={styles.content}>
+        <Slot />
+      </View>
+      <MenuApp />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F6F4FA",
+  },
+  content: {
+    flex: 1,
+  },
+});
