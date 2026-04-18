b7982e87f0c41d059fdaaed4e453a6acaa94daae
Joao Vitor <joaovd2006@gmail.com>
2026-04-18 20:19:51 -0300

    feat: termino da tela de login

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,41 @@
-import { Dimensions, Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { ActivityIndicator, Alert, Dimensions, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Logo from '../assets/images/file.jpg';
 
 export default function Login(){
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [loading,setLoading] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
+
+    async function getLogin(){
+      try{
+        setLoading(true)
+        if(!email || !password){
+          setLoading(false)
+          return Alert.alert("Atenção", "Informe os campos obrigatórios!")
+        }
+
+        setTimeout(()=>{
+          if(email == 'cognisus@gmail.com' && password == '123456'){
+            Alert.alert("Login permitido")
+            router.push("/")
+          }
+          else{
+            Alert.alert("Login invalido")
+          }
+          setLoading(false)
+          
+
+        },3000)
+
+      }catch (error){
+        console.log(error)
+      }
+    }
+
     return(
         <View style={style.container}>
             <View style={style.boxTop}>
@@ -11,10 +45,54 @@ export default function Login(){
                 />
             </View>
             <View style={style.boxMid}>
-                <Text style = {style.texto}>E-mail ou usuário</Text>
-                <TextInput/>
-                <Text style = {style.texto}>sua senha</Text>
-                <TextInput/>
+                <Text></Text>
+                <View style ={style.boxInput}>
+                  <TextInput
+                    placeholder="Email ou usuário"
+                    style={style.input}
+                    value ={email}
+                    onChangeText={setEmail}
+                  />
+                  <Ionicons style ={style.icons}
+                        name='mail'
+                        size={24}
+                        
+
+                      />
+                </View>
+                
+                <View style ={style.boxInput}>
+                  <TextInput
+                    placeholder="Senha"
+                    style={style.input}
+                    secureTextEntry={!showPassword}
+                    value ={password}
+                    onChangeText={setPassword}
+                    
+                  />
+                  <Pressable onPress={() => setShowPassword(!showPassword)}>
+                      <Ionicons style ={style.icons}
+                        name={showPassword? 'eye-off':'eye'}
+                        size={24}
+                        
+
+                      />
+                  </Pressable>
+                </View>
+                <View style={style.boxBotton}>
+                  <TouchableOpacity style ={style.button} onPress={()=>getLogin()}>
+                    {
+                      loading?
+                      <ActivityIndicator color={'white'} size={'small'}/>
+                      :
+                      <Text style = {style.textButton}>Entrar</Text>
+                    }
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Text style = {style.secondaryButton}>Cadastrar novo usuário</Text>
+                  </TouchableOpacity>
+                  
+                </View>
             </View>
         </View>
     )
@@ -34,10 +112,47 @@ const style = StyleSheet.create({
     alignItems:"center"
   },
   boxMid: {
-    height:Dimensions.get('window').height/3,
+    height:Dimensions.get('window').height/2.5,
     width:'100%',
     marginTop: -100,
     backgroundColor:"#e3deff",
+    borderRadius: 20,
+
+  },
+  boxBotton: {
+    height:50,
+    width:'100%',
+    alignSelf: "center",
+    borderRadius: 10,
+    marginTop:10,
+
+  },
+  boxInput: {
+    height:50,
+    width:'85%',
+    alignSelf: "center",
+    borderWidth:1,
+    borderRadius: 10,
+    marginTop:30,
+    flexDirection:'row-reverse',
+    paddingHorizontal:-20
+
+  },
+  button: {
+    height:50,
+    width:'85%',
+    marginTop: 10,
+    alignSelf: "center",
+    backgroundColor:"#732cad",
+    borderRadius: 20,
+
+  },
+  secondaryButton: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "#4d62da",
+    alignSelf: "center",
+    marginTop: 20,
 
   },
 
@@ -46,11 +161,23 @@ const style = StyleSheet.create({
     height: 150,
     borderRadius: 75, // metade de 150
   },
-  texto: {
-    fontSize: 16,
-    color: "#64748B",
-    textAlign: "center",
+
+  textButton: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "#ffffff",
+    alignSelf: "center",
     marginTop: 12,
-    marginBottom: 32,
+  },
+  input:{
+    flex: 1,
+    height:"100%",
+    width:"100%",
+  
+  },
+  icons:{
+    marginTop:11,
+    marginLeft:5, 
+   
   }
 });
