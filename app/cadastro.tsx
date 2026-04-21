fd6616f9c23e693a7f21853341942714abed9b7c
Joao Vitor <joaovd2006@gmail.com>
2026-04-20 22:01:08 -0300

    feat: termino da tela de cadastro

--- app/cadastro.tsx
+++ app/cadastro.tsx
@@ -1,10 +1,50 @@
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React, { useState } from "react";
-import { Dimensions, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Alert, Dimensions, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Logo from '../assets/images/file.jpg';
 
 export default function Cadastro(){
+    const [name,setName] = useState('')
+    const [cpf,setCpf] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [confirmPassword,setConfirmPassword] = useState('')
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    async function getCadastro(){
+      try{
+        setLoading(true)
+        if(!email || !password || !confirmPassword || !cpf || !name){
+          setLoading(false)
+          return Alert.alert("Atenção", "Informe os campos obrigatórios!")
+        }
+        else if (!email.includes("@")) {
+            setLoading(false)
+            return Alert.alert("Erro", "Email inválido")
+        }
+        else if (password.length < 6) {
+            setLoading(false)
+            return Alert.alert("Erro", "Senha deve ter pelo menos 6 caracteres")
+        }
+        else if (password!=confirmPassword) {
+            setLoading(false)
+            return Alert.alert("Erro", "Senha e confirmar senha estão diferentes")
+        }
+
+        setTimeout(()=>{
+          Alert.alert("Sucesso", "Cadastro realizado!")
+          router.back()
+          setLoading(false)
+          
+
+        },3000)
+
+      }catch (error){
+        console.log(error)
+      }
+    }
 
     return(
         <View style={style.container}>
@@ -19,7 +59,10 @@ export default function Cadastro(){
                 <View style ={style.boxInput}>
                     <TextInput 
                     placeholder = "Digite seu Nome"
+                    autoCapitalize="words"
                     style={style.input}
+                    value={name}
+                    onChangeText={setName}
                     />
                     <AntDesign style ={style.icons}
                         name='smile'
@@ -31,7 +74,11 @@ export default function Cadastro(){
                 <View style ={style.boxInput}>
                     <TextInput 
                     placeholder = "Digite seu CPF (somente numeros)"
+                    keyboardType="numeric"
                     style={style.input}
+                    value={cpf}
+                    maxLength={11}
+                    onChangeText={setCpf}
                     />
                     <FontAwesome style ={style.icons}
                         name='id-card-o'
@@ -43,7 +90,11 @@ export default function Cadastro(){
                 <View style ={style.boxInput}>
                     <TextInput 
                         placeholder = "Digite seu E-mail"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
                         style={style.input}
+                        value={email}
+                        onChangeText={setEmail}
                     />
                     <Ionicons style ={style.icons}
                         name='mail'
@@ -55,8 +106,11 @@ export default function Cadastro(){
                 <View style ={style.boxInput}>
                     <TextInput 
                     placeholder = "Digite sua senha"
+                    autoCapitalize="none"
                     style={style.input}
                     secureTextEntry={!showPassword}
+                    value={password}
+                    onChangeText={setPassword}
                     />
                     <Pressable onPress={() => setShowPassword(!showPassword)}>
                       <Ionicons style ={style.icons}
@@ -70,8 +124,11 @@ export default function Cadastro(){
                 <View style ={style.boxInput}>
                     <TextInput 
                     placeholder = "Confirmar Senha"
+                    autoCapitalize="none"
                     style={style.input}
                     secureTextEntry={!showPassword}
+                    value ={confirmPassword}
+                    onChangeText={setConfirmPassword}
                     />
                     <Pressable onPress={() => setShowPassword(!showPassword)}>
                       <Ionicons style ={style.icons}
@@ -85,11 +142,16 @@ export default function Cadastro(){
             
             </View>
             <View style = {style.boxBotton}>
-                    <TouchableOpacity style = {style.button}>
+                    <TouchableOpacity style = {style.button} onPress={() => router.back()}>
                         <Text style={style.textButton}> voltar</Text>
                     </TouchableOpacity >
-                    <TouchableOpacity style = {style.button}>
+                    <TouchableOpacity style = {style.button} onPress={getCadastro}>
+                        {
+                        loading?
+                        <ActivityIndicator color={'white'} size = {'small'}/>
+                        :
                         <Text style={style.textButton}>confirmar</Text>
+                        }
                     </TouchableOpacity>
 
             </View>
@@ -127,6 +189,7 @@ const style = StyleSheet.create({
     borderRadius: 10,
     marginTop:13,
     flexDirection:'row-reverse',
+    paddingHorizontal: 10
 
   },
   boxBotton: {
