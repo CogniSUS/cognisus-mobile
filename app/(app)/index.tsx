058a5b4747abd364f1d8a24c600fc2d3efcb5ed1
Joao Vitor <joaovd2006@gmail.com>
2026-05-03 00:10:48 -0300

    feat: frontend da tela de cadastro do paciente

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -3,16 +3,49 @@ import { AntDesign, FontAwesome, FontAwesome5, Ionicons } from "@expo/vector-ico
 import { Picker } from '@react-native-picker/picker';
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
-import { Dimensions, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Alert, Dimensions, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function HomePage() {
   const [isDbReady, setIsDbReady] = useState(false)
   const [mostrarCadastro, setMostrarCadastro] = useState(false)
+  const [nome, setNome] = useState('')
+  const [cpf,setCpf] = useState('')
+  const [dataNascimento, setDataNascimento] = useState('')
   const [sexo, setSexo] = useState('')
   const [escolaridade, setEscolaridade] = useState('')
   const [dcnt, setDCNT] = useState('')
   const [unidadeSaude, setUnidadeSaude] = useState('')
+  const [loading, setLoading] = useState(false);
 
+  async function cadastrarPaciente(){
+    try{
+      setLoading(true)
+      if (!nome ||!cpf|| !dataNascimento || sexo=="" || dcnt=="" || unidadeSaude=="" || escolaridade=="") {
+        setLoading(false)
+        return Alert.alert(
+          "Erro",
+          "Preencha os campos"
+        )
+      
+      } else if (cpf.length != 11) {
+              setLoading(false);
+              return Alert.alert("Erro", "CPF deve conter exatamente 11 dígitos");}
+              
+      setTimeout(()=>{
+          Alert.alert("Sucesso", "Cadastro de paciente realizado!")
+          setMostrarCadastro(false)
+          setLoading(false)
+          
+
+        },3000)
+
+    }
+    catch(error){
+      console.log(error);
+      Alert.alert("Erro", "Erro ao conectar ao servidor.");
+      setLoading(false);
+    }
+  }
   useEffect(() => {
     const initializeDB = async () => {
       await initDB();
@@ -44,15 +77,49 @@ export default function HomePage() {
                 <View style={styles.boxInput}>
                   <TextInput
                     placeholder="Nome completo"
+                    value={nome}
+                    onChangeText={setNome}
                     autoCapitalize="words"
                     style={styles.input}
                   />
                   <AntDesign style={styles.icons} name="smile" size={24} />
               </View>
+                      <View style={styles.boxInput}>
+                        <TextInput
+                          placeholder="Digite seu CPF (somente numeros)"
+                          keyboardType="numeric"
+                          style={styles.input}
+                          value={cpf}
+                          maxLength={11}
+                          onChangeText={setCpf}
+                        />
+                        <FontAwesome style={styles.icons} name="id-card-o" size={24} />
+                      </View>
                 <View style={styles.boxInput}>
                   <TextInput
-                    placeholder="Idade"
+                    placeholder="Data de nascimento"
+                    value={dataNascimento}
+                    onChangeText={(text) => {
+                        let formatted = text.replace(/\D/g, '')
+
+                        if (formatted.length > 2) {
+                          formatted =
+                            formatted.slice(0, 2) +
+                            '/' +
+                            formatted.slice(2)
+                        }
+
+                        if (formatted.length > 5) {
+                          formatted =
+                            formatted.slice(0, 5) +
+                            '/' +
+                            formatted.slice(5)
+                        }
+
+                        setDataNascimento(formatted)
+                      }}
                     keyboardType="numeric"
+                     maxLength={10}
                     style={styles.input}
                   />
                   <FontAwesome5 style={styles.icons} name="calendar-alt" size={24} />
@@ -141,16 +208,20 @@ export default function HomePage() {
                 </View>
                 <View style={styles.boxBotton}>
                   <TouchableOpacity
-                    style={[styles.button, styles.TertiaryButton]}
+                    style={[styles.button, styles.tertiaryButton]}
                     onPress={() => setMostrarCadastro(false)}
                   >
                     <Text style={styles.tertiaryButtonText}>Voltar</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    style={[styles.button, styles.TertiaryButton]}
+                    style={[styles.button, styles.tertiaryButton]} onPress={cadastrarPaciente}
                   >
-                    <Text style={styles.tertiaryButtonText}>cadastrar</Text>
+                    {loading ? (
+                                <ActivityIndicator color={"white"} size={"small"} />
+                              ) : (
+                                <Text style={styles.tertiaryButtonText}>confirmar</Text>
+                              )}
                 </TouchableOpacity>
               </View>
             </View>
@@ -191,27 +262,27 @@ const styles = StyleSheet.create({
     boxTop: {
       height: Dimensions.get("window").height / 5.3,
       width: "100%",
-      marginTop: -65,
+      marginTop: -43,
       alignItems: "center",
     },
     boxMid: {
-      height: Dimensions.get("window").height / 1.5,
+      height: Dimensions.get("window").height / 1.4,
       width: "100%",
       marginTop: -100,
       backgroundColor: "#e3deff",
       borderRadius: 20,
     },
     boxBotton:{
-    height: 65,
+    height: 62,
     width: "85%",
     alignSelf: "center",
     marginTop: 15,
     },
-    TertiaryButton: {
+    tertiaryButton: {
       backgroundColor: "#732cad"
     },
     boxInput: {
-      height: 55,
+      height: 51,
       width: "85%",
       alignSelf: "center",
       borderWidth: 1,
