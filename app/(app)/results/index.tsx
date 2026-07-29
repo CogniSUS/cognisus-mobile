d1dd0323a89ad58ebff39cdaa84a465ce8b84c42
Joao Vitor <joaovd2006@gmail.com>
2026-07-29 19:33:15 -0300

    feat: Design da tela de resultado

--- app/(app)/results/index.tsx
+++ app/(app)/results/index.tsx
@@ -1,28 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { router, useFocusEffect } from "expo-router";
 
 export default function ResultsPage() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Resultados</Text>
-      <Text style={styles.text}>Tela de resultados.</Text>
-    </View>
-  );
+  useFocusEffect(() => {
+      router.replace("/results/resultado");
+    });
+
+    return null;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F8FAFC",
-    padding: 24,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "700",
-    color: "#0F172A",
-    marginBottom: 8,
-  },
-  text: {
-    fontSize: 16,
-    color: "#64748B",
-  },
-});
