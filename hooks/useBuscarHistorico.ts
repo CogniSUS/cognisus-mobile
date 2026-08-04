7bc2bc2327f00ee74fc346bf06e48b66263841ee
Joao Vitor <joaovd2006@gmail.com>
2026-08-04 15:40:41 -0300

    fix: aplicar ajustes de toast e loading na busca de historico

--- hooks/useBuscarHistorico.ts
+++ hooks/useBuscarHistorico.ts
@@ -1,30 +1,35 @@
 import buscarPorCpf from "@/database/repositories/pacienteRepository";
 import { router } from "expo-router";
 import { useState } from "react";
-import { Alert } from "react-native";
+import { useToast } from "./useToast";
 
 export function useBuscaHistorico(){
-    const [cpfBusca, setCpfBusca] = useState("");
+        const [cpfBusca, setCpfBusca] = useState("");
+        const { info, error } = useToast();
         const [loading, setLoading] = useState(false);
         async function getBuscarHistorico(){
           try {
             setLoading(true)
             const cpfLimpo = cpfBusca.replace(/\D/g, "")
             if(cpfLimpo.length!==11){
-              return Alert.alert("Aviso", "CPF precisa ter 11 números")
+              setLoading(false)
+              info("CPF precisa ter 11 números")
+              return
             }
     
             const paciente = await buscarPorCpf(cpfLimpo)
             
             if(!paciente){
-              return Alert.alert("Paciente não encontrado");
+              setLoading(false)
+              error("Paciente não encontrado")
+              return
             }
             //funcionalidade ainda não implementada
             //é necessario a tela de histórico do paciente
             router.push("/(app)/informations")
-          } catch (error) {
-            console.log(error)
-            Alert.alert("Ocorreu um erro ")
+          } catch (err) {
+            console.log(err)
+            error("Ocorreu um erro ")
           } finally{
             setLoading(false)
           }
