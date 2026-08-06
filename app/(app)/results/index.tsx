f43b0092b5385bdec04f171f4ab144a6756018f5
Italo Chiaradia <italo_ide@id.uff.br>
2026-08-06 18:44:34 -0300

    refactor: improve layout and styling in ResultsPage component

--- app/(app)/results/index.tsx
+++ app/(app)/results/index.tsx
@@ -2,41 +2,56 @@ import { useBuscaHistorico } from "@/hooks/useBuscarHistorico";
 import { formatCpf } from "@/utils/formatters";
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { ActivityIndicator, Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function ResultsPage(){
-    const { cpfBusca, setCpfBusca, loading, getBuscarHistorico, } = useBuscaHistorico();
-    return(
-        <View style={styles.container}>
-            <View style={styles.boxTop}>
-              <Text style={styles.title}>Histórico do Paciente</Text>
-              <Text style={styles.text}>Digite o CPF para acessar o histórico completo de avaliações</Text>  
-            </View>
-            <View style={styles.boxMid}>
-              <View style={styles.boxInput}>
-                <Ionicons name="search-outline" size={20} color="#94A3B8" />
-                <TextInput
-                  placeholder="Digite o CPF do paciente"
-                  placeholderTextColor="#9CA3AF"
-                  keyboardType="numeric"
-                  value={cpfBusca}
-                  style={styles.input}
-                  maxLength={14}
-                  onChangeText={(text) => setCpfBusca(formatCpf(text))}/>
-              </View>
-              <TouchableOpacity style={styles.button} onPress={getBuscarHistorico}>
-                {loading ?(
-                  <ActivityIndicator color={"white"} size={"small"} />
-                ):(
-                  <Text style={styles.textButton}>Buscar Histórico</Text>
-                )}
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.secundaryButton} onPress={()=>router.back()}>
-                <Text style={styles.secundaryTextButton}>Voltar</Text>
-              </TouchableOpacity>
-            </View>
+export default function ResultsPage() {
+  const { cpfBusca, setCpfBusca, loading, getBuscarHistorico } =
+    useBuscaHistorico();
+  return (
+    <View style={styles.container}>
+      <View style={styles.boxTop}>
+        <Text style={styles.title}>Histórico do Paciente</Text>
+        <Text style={styles.text}>
+          Digite o CPF para acessar o histórico completo de avaliações
+        </Text>
+      </View>
+      <View style={styles.boxMid}>
+        <View style={styles.boxInput}>
+          <Ionicons name="search-outline" size={20} color="#94A3B8" />
+          <TextInput
+            placeholder="Digite o CPF do paciente"
+            placeholderTextColor="#9CA3AF"
+            keyboardType="numeric"
+            value={cpfBusca}
+            style={styles.input}
+            maxLength={14}
+            onChangeText={(text) => setCpfBusca(formatCpf(text))}
+          />
         </View>
-    );
+        <TouchableOpacity style={styles.button} onPress={getBuscarHistorico}>
+          {loading ? (
+            <ActivityIndicator color={"white"} size={"small"} />
+          ) : (
+            <Text style={styles.textButton}>Buscar Histórico</Text>
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.secundaryButton}
+          onPress={() => router.back()}
+        >
+          <Text style={styles.secundaryTextButton}>Voltar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -49,7 +64,7 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height / 4,
     width: "100%",
     alignItems: "center",
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
   boxMid: {
     height: Dimensions.get("window").height / 3,
@@ -61,10 +76,13 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: "#0F172A",
     marginBottom: 8,
+    textAlign: "center",
   },
   text: {
-    fontSize: 12,
+    fontSize: 14,
     color: "#64748B",
+    textAlign: "center",
+    paddingHorizontal: 20,
   },
   boxInput: {
     height: 50,
@@ -76,13 +94,14 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderColor: "#732cad",
     flexDirection: "row",
-    paddingHorizontal: -20,
+    paddingHorizontal: 16,
   },
   input: {
     flex: 1,
     height: "100%",
     color: "#172033",
     fontSize: 16,
+    marginLeft: 10,
   },
   button: {
     height: 50,
@@ -91,6 +110,7 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     backgroundColor: "#732cad",
     borderRadius: 20,
+    justifyContent: "center",
   },
   secundaryButton: {
     height: 50,
@@ -99,21 +119,19 @@ const styles = StyleSheet.create({
     alignSelf: "center",
     borderRadius: 20,
     borderColor: "#7a7a7a",
-    borderWidth:1,
+    borderWidth: 1,
+    justifyContent: "center",
   },
   textButton: {
     fontSize: 20,
     fontWeight: "500",
     color: "#ffffff",
     alignSelf: "center",
-    marginTop: 12,
   },
   secundaryTextButton: {
     fontSize: 20,
     fontWeight: "500",
     color: "#2e2d2d",
     alignSelf: "center",
-    marginTop: 12,
-  }
- 
+  },
 });
